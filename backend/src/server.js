c93fb9d7cd64d0ea72e7028a4dad89c010ea3fe9
b09901042@ntu.edu.tw <kkyzl>
2023-06-12 18:27:22 +0800

    add function of starting & ending votes. move data to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import router from "./iota/route";
 import { Client, hexToUtf8, initLogger, utf8ToHex } from "@iota/client";
+import voting from "./iota/voting";
 
 // import { Tag } from "@iota/core";
 require("dotenv").config({ path: "../.env" });
@@ -15,4 +16,5 @@ app.use("/", router);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is up onport ${port}.`);
+  voting(1, data[1].lastBlockID);
 });
